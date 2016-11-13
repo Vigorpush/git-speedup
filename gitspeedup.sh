4eda7c34cb1db9e7351f36e53b6636b07aa33ec8
Vigor <Vigorzjw@gmail.com>
2016-11-13 02:05:42 -0600

    debuged

--- gitspeedup.sh
+++ gitspeedup.sh
@@ -1,7 +1,12 @@
 echo "Start To Speed Up Git On your Windows Machine!"
 
+
+git repack -a -d --depth=250 --window=250 
+# The default was not to change the window or depth at all.  As suggested 
+     # by Jon Smirl, Linus Torvalds and others, default to 
+
 echo "Activate Git's own tracing"
-GIT_TRACE=1 git stash #Activate Git's own tracing
+#GIT_TRACE=1 git stash #Activate Git's own tracing
 
 echo "Enable git preload file index"
 git config --global core.preloadindex true #Enable git preload file index
@@ -16,7 +21,7 @@ echo "Cleanup unnecessary files and optimize the local repository"
 git config --global gc.auto 256 #Set auto clean to be 256, to save memory
 
 echo "Enable inspecting small working trees' modification times"
-git config core.ignoreStat false
+git config --global core.ignoreStat true # 
 
 # These can be worked around by running git update-index --really-refresh after performing such operations. I hope that git add will be changed to stage changes to assume-unchanged files, which would remove this only complication
 
