0ee5834962faad8596b8b3a14d16ab7b8dd187ab
Vigor <Vigorzjw@gmail.com>
2016-11-13 00:05:55 -0600

    added new

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # git-speedup
 Speed up git bash on windows
 # How to use this
+I found the following change on a StackOverflow post that seems to work way faster:
+
+Go to your git install directory (Mine is c:\Program Files (x86)\Git
+In the etc folder, open the file 'profile' in a text editor
+This file is executed when the bash shell is initialized, you can do a lot of customization in here
+Near the bottom, you will find some commands related to setting a variable called PS1 (PS1 = ...)
+Either replace it or just append the following lines after it
+
+
+fast_git_ps1 ()                                                                              
+{                                                                                            
+    printf -- "$(git branch 2>/dev/null | grep -e '\* ' | sed 's/^..\(.*\)/ {\1} /')"    
+}                                                                                            
+
+PS1='\[\033]0;$MSYSTEM:\w\007                                                                
+\033[32m\]\u@\h \[\033[33m\w$(fast_git_ps1)\033[0m\]                                         
+$
--- gitspeedup.sh
+++ gitspeedup.sh
@@ -1,18 +1,35 @@
 echo "Start To Speed Up Git On your Windows Machine!"
 
+
+echo "Activate Git's own tracing"
+GIT_TRACE=1 git stash #Activate Git's own tracing
+
 echo "Enable git preload file index"
 git config --global core.preloadindex true #Enable git preload file index
 
-echo "Enable git cache filesystem "
+echo "Enable Global git cache filesystem "
 git config --global core.fscache true #Enable the filesystem cache
+
+echo "Enable Local git cache filesystem "
+git config core.fscache true
 
 echo "Cleanup unnecessary files and optimize the local repository"
 git config --global gc.auto 256 #Set auto clean to be 256, to save memory
 
 echo "Avoid inspecting large working trees' modification times"
-git config --global core.ignoreStat true
+# git config --global core.ignoreStat true
 
-echo "Activate Git's own tracing"
-GIT_TRACE=1 git stash #Activate Git's own tracing
+# This feature works quite well with git-annex. Especially because git annex's files are immutable, so aren't going to change out from under it, this is a nice fit. If you have a very large tree and git status is annoyingly slow, you can turn it on:
+
+echo "If you are working on large working tree, then use"
+
+# https://git-annex.branchable.com/tips/assume-unstaged/
+
+
+# This is useful on systems where lstat() calls are very slow, such as CIFS/Microsoft Windows.
+
+# False by default.
+
+
 
 echo "Finishing Optimazing Git"
