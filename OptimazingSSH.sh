91e36d26c98fbef1f62bce76a4b6f92c8b7244c5
Vigor <Vigorzjw@gmail.com>
2016-11-13 03:15:27 -0600

    Added new sh file which will optimazing SSH

---
+++ OptimazingSSH.sh
@@ -0,0 +1,16 @@
+printf "Start to SSH Optimazing"
+cat ~/.ssh/config
+
+echo "Enables the sharing of multiple SSH sessions over a single network connection, and auto-creating a master connection if it does not already exist."
+
+printf 'ControlMaster auto \n' >~/.ssh/config
+
+echo "specifies the path to the control socket used for connection sharing. %r will be substituted by the remote login username, %h by the target host name and %p by the port."
+
+echo 'ControlPath /tmp/%r@%h:%p' >>~/.ssh/config
+echo "keeps the master connection open in the background indefinitely"
+
+echo 'ControlPersist yes \n' >>~/.ssh/config
+printf "Finished Optimazing SSH "
+
+
